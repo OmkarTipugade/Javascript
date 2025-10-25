34894a0bb435cbefab21973a39978d8369268c0a
Omkar  Tipugade <113380425+OmkarTipugade@users.noreply.github.com>
2025-10-25 17:13:39 +0000

    Datatype in js

---
+++ 01_Basics/01_Datatypes.js
@@ -0,0 +1,17 @@
+const bigNumber = 1234n;
+
+console.log(typeof bigNumber)
+
+const myFunc = () => {
+    console.log("My function")
+}
+
+console.log(typeof myFunc)
+
+const obj = {
+    fname: "Omkar",
+    lname: "Tipugade",
+    prnNo: 20834980232
+}
+
+console.log(typeof obj)
