bdab0c986b666a05a7b3034353bc20b722932d78
Omkar  Tipugade <113380425+OmkarTipugade@users.noreply.github.com>
2025-10-29 05:59:01 +0000

    Arrays in js

---
+++ 01_Basics/05_array.js
@@ -0,0 +1,12 @@
+const arr = [1,3,4,5]
+
+const arr2 = ["ninja","sumoo"]
+
+const myarr = new Array(4, 5,6, 8,9)
+
+console.log(`A ${arr} ${arr2} ${myarr}`)
+
+console.log(`B ${arr.slice(0,2)} ${arr}`)
+
+console.log(`C ${arr.splice(0,2)} ${arr}`)
+
